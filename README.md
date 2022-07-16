cb3fc4bec34c9ad09f6f9f9b5514aea7d0855da5
Francois Harewood <francois.harewood@gmail.com>
2022-07-15 23:30:32 -0500

    some minor styling to patrol lists

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 This is the repository for the 1st Trinidad Sea Scouts website 1tss.org. Any questions or comments about the website can be emailed to me at kevanleelum@gmail.com
 
-Run sass compiler when changing style files:
 
-sass --watch styles/main.scss master.css
--- patrols.js
+++ patrols.js
@@ -9,16 +9,18 @@ scouts.patrols.map(function(patrol){
     input+= `
     <div class="patrol">
         <h1>${patrol.title}</h1>
-        <div class="patrol_leader_cont">
-            <h2>Patrol Leader</h2>
-            <img src="${patrol.patrolLeader.photo}" height="200px" alt="patrol leader">
-            <h3>${patrol.patrolLeader.name}</h3>
-        </div>
-        <div class="patrol_names_cont">
-            <h3>Patrol</h3>
-            <ul>
-                ${patrol.patrol.map(y => `<li>${y}</li>`).join('')}
-            </ul>
+        <div class="patrol_wrapper">
+            <div class="patrol_leader_cont">
+                <h2>Patrol Leader</h2>
+                <img src="${patrol.patrolLeader.photo}" height="200px" alt="patrol leader">
+                <h3>${patrol.patrolLeader.name}</h3>
+            </div>
+            <div class="patrol_names_cont">
+                <h3>Patrol</h3>
+                <ul>
+                    ${patrol.patrol.map(y => `<li>${y}</li>`).join('')}
+                </ul>
+            </div>
         </div>
     </div>
     `;
